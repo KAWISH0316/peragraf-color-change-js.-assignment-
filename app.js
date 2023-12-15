22f97440f1e17510fc49a1bb905d0b1e2be9aafb
KAWISH0316 <kawishpc1122@gmail.com>
2023-12-15 21:07:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function change1(){
+    var perag = document.getElementById(peragraf1)
+    peragraf1.style.color = 'red'
+}
+
+function change2(){
+    var perag = document.getElementById(peragraf2)
+    peragraf2.style.color = 'rgb(60, 255, 0'
+}
+
+function change3(){
+    var perag = document.getElementById(peragraf3)
+    peragraf3.style.color = 'rgb(0, 200, 255)'
+}
